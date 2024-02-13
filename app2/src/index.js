272b0dd2d398ddacf0097e4f80f4899ef57baf5a
krunalbalar10 <bholu2104patel@gmail.com>
2024-02-13 13:47:01 +0530

    router library installed

--- app2/src/index.js
+++ app2/src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import {BrowserRoutes , Routes , Route, BrowserRouter} from 'react-router-dom';
+let Home = () => <div><h1>Home Page</h1></div>
+let Aboutus = () => <div><h1>About Us Page</h1></div>
+let Contact = () => <div><h1>Contact Page</h1></div>
+let PageNotFound = () =>  <div><h1> oops, no such page exists.  </h1> <hr/> 404 - error</div>;
+function App (){
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route index path="/" element={<Home/>}/>
+        <Route path="/aboutus" element={<Aboutus/>}/>
+        <Route path="/contactus" element={<Contact/>}/>
+        <Route path="/*" element={<PageNotFound/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App/>);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
